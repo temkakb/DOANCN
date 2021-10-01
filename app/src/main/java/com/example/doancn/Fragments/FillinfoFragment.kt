8c33b4d676dc7b4389399c064bdd79db422e7cd1
Duong Nguyen Minh Tan <dnm.tan.2000@gmail.com>
2021-10-01 10:58:01 +0700

    wating for dai

--- app/src/main/java/com/example/doancn/Fragments/FillinfoFragment.kt
+++ app/src/main/java/com/example/doancn/Fragments/FillinfoFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.doancn.R
 
-class FillinfoFragment :Fragment() {
+open class FillinfoFragment :Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
