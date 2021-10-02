848367011e7ecc3d93c28f6bf661cc543f777b39
Duong Nguyen Minh Tan <dnm.tan.2000@gmail.com>
2021-10-02 21:11:24 +0700

    user match

--- app/src/main/java/com/example/doancn/Models/User.kt
+++ app/src/main/java/com/example/doancn/Models/User.kt
@@ -1,4 +1,11 @@
 package com.example.doancn.Models
 
-class User {
+class User (){
+    lateinit var phoneNumber : String
+    lateinit var name : String
+    lateinit var address : String
+    lateinit var currentWorkPlace : String
+    lateinit var educationLevel : String
+    lateinit var dob : String
+     var gender : String? = null
 }
