cfede0c57eb14f81e0811f8ba579d96f28ff6855
Duong Nguyen Minh Tan <dnm.tan.2000@gmail.com>
2021-11-06 13:45:32 +0700

    finish generate qr code

--- app/src/main/java/com/example/doancn/Fragments/MyClass/MyClassFragment.kt
+++ app/src/main/java/com/example/doancn/Fragments/MyClass/MyClassFragment.kt
@@ -1,29 +1,46 @@
 package com.example.doancn.Fragments.MyClass
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.doancn.R
-import com.example.doancn.Repository.AttendanceRepository
-import com.example.doancn.Utilities.TokenManager
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.example.doancn.Utilities.QrCodeManager
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+
 
 class MyClassFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val repository = AttendanceRepository()
-        GlobalScope.launch {
-            val qrcode = repository.getQrcodeToken(2L,TokenManager.userToken)
-            Log.d("sdsasdasdasdadasd",qrcode.qrId)
-        }
+
+
 
         return inflater.inflate(R.layout.fragment_myclass, container, false)
+    }
+
+
+    private fun doAttandance (){
+        val barcodeLauncher = registerForActivityResult(
+            ScanContract()
+        ) { result: ScanIntentResult ->
+            if (result.contents == null) {
+                Toast.makeText(requireContext(), "Hủy quét", Toast.LENGTH_LONG).show()
+            } else { // diem danh
+
+            }
+        }
+        val options = ScanOptions()
+        options.setBeepEnabled(false)
+        options.setPrompt("quét mã QR để tiến hành điểm danh")
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        barcodeLauncher.launch(options)
     }
 }
--- app/src/main/java/com/example/doancn/Utilities/QrCodeManager.kt
+++ app/src/main/java/com/example/doancn/Utilities/QrCodeManager.kt
@@ -1,4 +1,89 @@
 package com.example.doancn.Utilities
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.CountDownTimer
+import android.util.Log
+import android.widget.Toast
+import com.example.doancn.Models.QrCodeX
+import com.example.doancn.R
+import com.example.doancn.Repository.AttendanceRepository
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import kotlinx.android.synthetic.main.dialog_qrcode.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import retrofit2.HttpException
+import java.util.concurrent.TimeUnit
+
 object QrCodeManager {
+    val repository = AttendanceRepository()
+    const val URL :String = ""
+    var qrcode : QrCodeX? = null
+    var multiFormatWriter : MultiFormatWriter = MultiFormatWriter()
+    val encode = BarcodeEncoder()
+
+    fun getQrCode (context : Context) {
+
+            GlobalScope.launch(Dispatchers.IO) {
+                try {
+                    qrcode = repository.getQrcodeToken(2L, TokenManager.userToken)
+                    withContext(Dispatchers.Main) {
+                        showDialog(context)
+                    }
+                } catch (e: HttpException) {
+                    val jObjError = JSONObject(e.response()?.errorBody()!!.string())
+                    val msg = jObjError.get("message")
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(
+                            context,
+                            msg.toString(),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+    fun  showDialog(context: Context){
+        val dialog = Dialog(context)
+        dialog.setContentView(R.layout.dialog_qrcode)
+        dialog.btn_close.setOnClickListener {
+            dialog.dismiss()
+        }
+        Log.d("qrcodeid",qrcode!!.qrId)
+        val countDownTimer = object  : CountDownTimer (qrcode!!.timeleft.toLong()*1000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                dialog.countdown.text =context.resources.getString(R.string.formatted_time,TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)%60,
+                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished)%60)
+            }
+            override fun onFinish() {
+                dialog.dismiss()
+               val dialogalert = AlertDialog.Builder(context)
+                dialogalert.setTitle(context.resources.getString(R.string.qrcode_expired_title))
+                    .setMessage(context.resources.getString(R.string.qrcode_expired_msg))
+                    .setPositiveButton(context.resources.getString(R.string.qrcode_expired_yes)){ dialoginterface,Int ->
+                        getQrCode(context)
+                    }
+                    .setNegativeButton(context.resources.getString(R.string.qrcode_expired_no)){ dialoginterface,Int ->
+                        dialoginterface.dismiss()
+                    }.show()
+            }
+        }
+        try{
+            val bitMatrix = multiFormatWriter.encode(qrcode!!.qrId,BarcodeFormat.QR_CODE,700,700)
+            val bitmap=encode.createBitmap(bitMatrix)
+            dialog.iv_qrcode.setImageBitmap(bitmap)
+
+        }catch (e: WriterException){
+            e.printStackTrace()
+        }
+        countDownTimer.start()
+        dialog.show()
+    }
 }
